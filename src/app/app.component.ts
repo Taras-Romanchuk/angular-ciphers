12465ac7fdc3c159a7981947e7217d40904159c9
Taras-Romanchuk <9219462+Taras-Romanchuk@users.noreply.github.com>
2020-08-19 13:23:03 +0300

    Added draft Caesar cipher to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ciphers';
+  inputText: string;
+  outputText: string;
+  shift: number;
+
+  encrypt(formValues){
+    this.inputText = formValues.inputText;
+    this.shift = formValues.shift;
+    this.outputText = '';
+    for (var i = 0; i < this.inputText.length; i++){
+      if (this.inputText[i] === ' '){
+        this.outputText += ' ';
+      }
+      else{
+        var newCharCode = Number(this.inputText[i].charCodeAt(0)) + this.shift;
+        if (newCharCode < 97 || newCharCode > 122) {
+          newCharCode -= 26;
+        }
+        this.outputText += String.fromCharCode(newCharCode);
+      }
+    }
+  }
 }
